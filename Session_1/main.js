b48281b09c4a898966836f6b66dd9d917c478433
auradavid <aura.prisacariu@gmail.com>
2022-08-15 13:21:43 +0300

    Added prompt function

---
+++ Session_1/main.js
@@ -0,0 +1,23 @@
+let firstName = "Aura";
+let lastName = "David";
+let age = 32;
+let occupation = "pharmacist";
+let hobby = "reading";
+
+console.log (`My name is ${firstName} ${lastName}. 
+I'm ${age} and I'm currently working as a ${occupation}.
+My favorite way of spending my time is ${hobby}.`);
+
+function  myFunction () {
+    let answear = prompt("Hy! What is your name?", "Aura David");
+    if (answear != null) {
+        let user_input = document.getElementById("output-text");
+        user_input.innerHTML = answear;
+    }
+
+}
+
+console.log (answear);
+
+
+         
